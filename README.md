75276df3757ae4f2ba02802a0315e1fe0446882c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-09 06:27:38 +0000

    Developers data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Various reports generated from DevStats databases
 
 # Running reports
 
-- Developers: `PG_PASS=... ./sh/developers.sh 2016-01-01 2017-01-01`.
+- Developers details: `PG_PASS=... ./sh/developers.sh 2016-01-01 2017-01-01`.
+- Developers count: `PG_PASS=... ./sh/developers_count.sh 2016-01-01 2017-01-01`.
+- Developers list: `PG_PASS=... ./sh/developers_list.sh 2016-01-01 2017-01-01`.
---
+++ sql/developers_list.sql
@@ -0,0 +1,19 @@
+select
+  string_agg(sub.committer, ',') as developers
+from (
+  select distinct dup_committer_login as committer,
+    count(distinct sha) as commits
+  from
+    gha_commits
+  where
+    dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and dup_committer_login != ''
+    and (lower(dup_committer_login) {{exclude_bots}})
+  group by
+    dup_committer_login
+  order by
+    commits desc,
+    committer asc
+) sub
+;
