e22d590957d0d46e3ea47e558ce63d55deff5173
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-09 06:22:22 +0000

    First report
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # devstats-reports
 
 Various reports generated from DevStats databases
+
+# Running reports
+
+- Developers: `PG_PASS=... ./sh/developers.sh 2016-01-01 2017-01-01`.
---
+++ sh/developers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to set PG_PASS=..."
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide 1st argument date-from in YYYY-MM-DD format"
+  exit 2
+fi
+if [ -z "$2" ]
+then
+  echo "$0: you need to provide 2nd date-to in YYYY-MM-DD format"
+  exit 3
+fi
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 runq sql/developers.sql {{dtfrom}} "$1" {{dtto}} "$2" {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`"
