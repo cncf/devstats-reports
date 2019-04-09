74364f5a76cae7d1072db40b4011f90873bbce5a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-09 08:08:58 +0000

    Commits metric
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/commits.sql
+++ sql/commits.sql
@@ -1,10 +1,8 @@
 select
-  count(distinct dup_committer_login) as committers
+  count(distinct sha) as commits
 from
   gha_commits
 where
   dup_created_at >= '{{dtfrom}}'
   and dup_created_at < '{{dtto}}'
-  and dup_committer_login != ''
-  and (lower(dup_committer_login) {{exclude_bots}})
 ;
