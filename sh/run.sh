44272329e38f43b4581aff1db4a872c494209160
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-10 13:31:36 +0000

    Make passing additional script params possible
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sh/run.sh
+++ sh/run.sh
@@ -9,14 +9,14 @@ then
   echo "$0: you need to provide 1st argument - report type: developers, commits, prs, issues, contributing_companies_count etc"
   exit 2
 fi
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
   echo "$0: you need to provide 2nd argument date-from in YYYY-MM-DD format"
   exit 3
 fi
-if [ -z "$2" ]
+if [ -z "$3" ]
 then
   echo "$0: you need to provide 3rd date-to in YYYY-MM-DD format"
   exit 4
 fi
-GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 runq "sql/${1}.sql" {{dtfrom}} "$2" {{dtto}} "$3" {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`"
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 runq "sql/${1}.sql" {{dtfrom}} "${2}" {{dtto}} "${3}" {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`" ${@:4:99}
