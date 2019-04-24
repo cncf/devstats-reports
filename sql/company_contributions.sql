d01bd79dbcfdaef1e33e85447a30666949bc974d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-24 11:34:52 +0000

    Allow calculating company contributions for given periods and cumulative too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/company_contributions.sql
+++ sql/company_contributions.sql
@@ -13,7 +13,8 @@ from (
     and af.dt_from <= e.created_at
     and af.dt_to > e.created_at
   where
-    e.created_at < '{{dtto}}'
+    e.created_at >= '{{dtfrom}}'
+    and e.created_at < '{{dtto}}'
     and (lower(e.dup_actor_login) {{exclude_bots}})
     and e.type in (
       'PushEvent', 'PullRequestEvent', 'IssuesEvent',
