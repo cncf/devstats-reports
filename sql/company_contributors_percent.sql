84f02b2e42a65457d29a9b98954853319d397121
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-11 10:42:30 +0000

    Query updates [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sql/company_contributors_percent.sql
@@ -0,0 +1,27 @@
+select
+  sub.company_contributors as contributors,
+  (sub.company_contributors * 100.0) / sub.{{type}}_contributors as percent_contributors,
+  (sub.known_contributors * 100.0) / sub.all_contributors as data_quality
+from (
+  select
+    count(distinct e.dup_actor_login) as all_contributors,
+    count(distinct e.dup_actor_login) filter (where af.company_name = '{{company}}') as company_contributors,
+    count(distinct e.dup_actor_login) filter (where af.company_name is not null and e.dup_actor_login != '') as known_contributors
+  from
+    gha_events e
+  left join
+    gha_actors_affiliations af
+  on
+    e.actor_id = af.actor_id
+    and af.dt_from <= e.created_at
+    and af.dt_to > e.created_at
+  where
+    e.created_at >= '{{dtfrom}}'
+    and e.created_at < '{{dtto}}'
+    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and e.type in (
+      'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+      'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+    )
+) sub
+;
--- sql/contributors_count.sql
+++ sql/contributors_count.sql
@@ -1,12 +1,18 @@
 select
-  count(distinct dup_actor_login) as contributors
+  count(distinct e.dup_actor_login) as contributors
 from
-  gha_events
+  gha_events e
+left join
+  gha_actors_affiliations af
+on
+  e.actor_id = af.actor_id
+  and af.dt_from <= e.created_at
+  and af.dt_to > e.created_at
 where
-  created_at >= '{{dtfrom}}'
-  and created_at < '{{dtto}}'
-  and (lower(dup_actor_login) {{exclude_bots}})
-  and type in (
+  e.created_at >= '{{dtfrom}}'
+  and e.created_at < '{{dtto}}'
+  and (lower(e.dup_actor_login) {{exclude_bots}})
+  and e.type in (
     'PushEvent', 'PullRequestEvent', 'IssuesEvent',
     'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
   )
