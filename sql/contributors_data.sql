ed5788c83861ffb3fa6ffe3a72ff16b968a5024d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-24 05:10:51 +0000

    Update SQL for contributors data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/contributors_data.sql
+++ sql/contributors_data.sql
@@ -29,11 +29,23 @@ from (
       and (lower(dup_author_login) {{exclude_bots}})
       and dup_author_login != ''
       and author_id is not null
+    union select dup_committer_login as contributor,
+      committer_id as contributor_id,
+      event_id as contribution
+    from
+      gha_commits
+    where
+      dup_created_at >= '{{dtfrom}}'
+      and dup_created_at < '{{dtto}}'
+      and (lower(dup_committer_login) {{exclude_bots}})
+      and dup_committer_login != ''
+      and committer_id is not null
     ) sub
   left join
     gha_actors_emails ae
   on
     ae.actor_id = sub.contributor_id
+    and ae.email not like '%users.noreply.github.com'
   group by
     sub.contributor
   ) sub2
