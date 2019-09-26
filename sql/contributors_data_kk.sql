4ad99b2f9f932499e1d3c1c1ae0532c60fdb9340
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-26 20:14:42 +0000

    new report for k/k repo only
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sql/contributors_data_kk.sql
@@ -0,0 +1,60 @@
+select
+  sub2.*
+from (
+  select sub.contributor,
+    count(distinct sub.contribution) as contributions,
+    string_agg(distinct ae.email, ',') as emails
+  from (
+    select dup_actor_login as contributor,
+      actor_id as contributor_id,
+      id as contribution
+    from
+      gha_events
+    where
+      created_at >= '{{dtfrom}}'
+      and created_at < '{{dtto}}'
+      and (lower(dup_actor_login) {{exclude_bots}})
+      and type in (
+        'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+        'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+      )
+      and dup_repo_name = 'kubernetes/kubernetes'
+    union select dup_author_login as contributor,
+      author_id as contributor_id,
+      event_id as contribution
+    from
+      gha_commits
+    where
+      dup_created_at >= '{{dtfrom}}'
+      and dup_created_at < '{{dtto}}'
+      and (lower(dup_author_login) {{exclude_bots}})
+      and dup_author_login != ''
+      and author_id is not null
+      and dup_repo_name = 'kubernetes/kubernetes'
+    union select dup_committer_login as contributor,
+      committer_id as contributor_id,
+      event_id as contribution
+    from
+      gha_commits
+    where
+      dup_created_at >= '{{dtfrom}}'
+      and dup_created_at < '{{dtto}}'
+      and (lower(dup_committer_login) {{exclude_bots}})
+      and dup_committer_login != ''
+      and committer_id is not null
+      and dup_repo_name = 'kubernetes/kubernetes'
+    ) sub
+  left join
+    gha_actors_emails ae
+  on
+    ae.actor_id = sub.contributor_id
+    and ae.email not like '%users.noreply.github.com'
+  group by
+    sub.contributor
+  ) sub2
+where
+  sub2.contributions >= {{top_n}}
+order by
+  contributions desc,
+  sub2.contributor asc
+;
