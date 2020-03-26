a909abb8b1506fbfe9f3fd87fee45bce79211dc9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-26 07:00:52 +0000

    New report type
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sql/company_repo_group_stats.sql
@@ -0,0 +1,26 @@
+select
+  aa.company_name as company,
+  r.repo_group as project,
+  count(distinct e.id) as contributions,
+  count(distinct e.actor_id) as contributors
+from
+  gha_events e,
+  gha_repos r,
+  gha_actors_affiliations aa
+where
+  e.repo_id = r.id
+  and e.actor_id = aa.actor_id
+  and e.created_at >= aa.dt_from
+  and e.created_at < aa.dt_to
+  and e.type in (
+    'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+    'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+  )
+  and (lower(dup_actor_login) {{exclude_bots}})
+group by
+  aa.company_name,
+  r.repo_group
+order by
+  aa.company_name,
+  r.repo_group
+;
