5a6db9e171eba04873d50787e95961384b68c7a9
John DA <lukaszgryglicki@o2.pl>
2020-04-07 06:38:30 +0000

    Fixes
    Signed-off-by: John DA <lukaszgryglicki@o2.pl>

--- rep/contributions_stats.sh
+++ rep/contributions_stats.sh
@@ -9,4 +9,4 @@ then
   echo "$0: you need to set PG_DB=..., for example PG_DB=cii"
   exit 2
 fi
-GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_CSVOUT="data/${PG_DB}_contributions_stats.csv" runq util_sql/contributions_stats.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_CSVOUT="data/${PG_DB}_contributions_stats.csv" runq sql/contributions_stats.sql {{exclude_bots}} "`cat /etc/gha2db/util_sql/exclude_bots.sql`"
