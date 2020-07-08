20f3cec9ccea80263ddff044be418c1ba8289a48
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-08 06:48:10 +0000

    Add current stats
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/contributions_stats.sql
+++ sql/contributions_stats.sql
@@ -21,6 +21,31 @@ group by
   e.dup_actor_login,
   v1.title,
   v2.title
+union select
+  v.title || ' - now' as range,
+  e.dup_actor_login as author,
+  count(e.id) as contributions
+from
+  gha_events e, (
+  select title, time
+  from
+    sannotations
+  where
+    description like 'Kubernetes official release v1.1%'
+  order by
+    time desc
+  limit 1
+  ) v
+where
+  e.created_at >= v.time
+  and e.type in (
+    'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+    'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+  )
+  and (lower(e.dup_actor_login) {{exclude_bots}})
+group by
+  v.title,
+  e.dup_actor_login
 order by
   range asc,
   contributions desc,
