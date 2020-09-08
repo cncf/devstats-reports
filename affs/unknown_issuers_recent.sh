bb00b673ef9195fc6fe49a6ed83a875f5ee68b45
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-09-08 05:18:02 +0000

    Other affs related tasks recent mode
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ affs/unknown_issuers_recent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to set PG_PASS=..."
+  exit 1
+fi
+if [ -z "$PG_DB" ]
+then
+  echo "$0: you need to set PG_DB=..., for example PG_DB=cii"
+  exit 2
+fi
+if [ -z "${1}" ]
+then
+  echo "$0: you need to specify 'recent' period as a 1st arg, example '1 week'"
+  exit 3
+fi
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_CSVOUT="data/${PG_DB}_unknown_issuers_recent.csv" runq sql/unknown_issuers_recent.sql {{exclude_bots}} "`cat /etc/gha2db/util_sql/exclude_bots.sql`" {{ago}} "${1}"
