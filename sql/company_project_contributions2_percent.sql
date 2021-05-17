c66ca385f55558e914b755e8dd399c11f3045719
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-17 07:38:55 +0000

    Two sides of percentage stats - per project %, per company %
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/company_project_contributions2_percent.sql
+++ sql/company_project_contributions2_percent.sql
@@ -1,11 +1,11 @@
 -- this gives a project statistic - how many % a given company has in a given project
 select
-  -- sub.company_contributions as contributions
-  (sub.company_contributions * 100.0) / case sub.{{type}}_contributions when 0 then 1 else sub.{{type}}_contributions end as percent_contributions
+  -- sub.project_contributions as contributions
+  (sub.project_contributions * 100.0) / case sub.{{type}}_contributions when 0 then 1 else sub.{{type}}_contributions end as percent_contributions
   -- (sub.known_contributions * 100.0) / case sub.all_contributions when 0 then 1 else sub.all_contributions end as data_quality
 from (
-  select count(e.id) filter (where af.{{company_name}} = '{{company}}') as company_contributions,
-    count(e.id) filter (where af.{{company_name}} is not null) as known_contributions,
+  select count(e.id) filter (where r.repo_group = '{{project}}') as project_contributions,
+    count(e.id) filter (where r.repo_group is not null) as known_contributions,
     count(e.id) as all_contributions
   from
     gha_repos r,
@@ -21,7 +21,7 @@ from (
     and e.created_at < '{{dtto}}'
     and e.repo_id = r.id
     and e.dup_repo_name = r.name
-    and r.repo_group = '{{project}}'
+    and af.{{company_name}} = '{{company}}'
     and (lower(e.dup_actor_login) {{exclude_bots}})
     and e.type in (
       'PushEvent', 'PullRequestEvent', 'IssuesEvent',
