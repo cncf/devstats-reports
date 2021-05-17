eae728e60cfe8c30daf462436ef0a048778a3683
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-17 09:09:33 +0000

    2 new reports
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sql/company_country_contributors.sql
@@ -0,0 +1,30 @@
+select
+  count(distinct e.actor_id) as country_contributors
+from
+  gha_events e,
+  gha_actors a,
+  gha_countries c,
+  gha_actors_affiliations af
+where
+  e.actor_id = a.id
+  and e.dup_actor_login = a.login
+  and e.created_at >= '{{dtfrom}}'
+  and e.created_at < '{{dtto}}'
+  and (lower(e.dup_actor_login) {{exclude_bots}})
+  and e.type in (
+    'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+    'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+  )
+  and a.country_id is not null
+  and a.country_id != ''
+  and a.country_id = c.code
+  and c.name = '{{country}}'
+  and e.actor_id = af.actor_id
+  and af.dt_from <= e.created_at
+  and af.dt_to > e.created_at
+  and af.{{company_name}} = '{{company}}'
+union select 0 as country_contributors
+order by
+  country_contributors desc
+limit 1
+;
