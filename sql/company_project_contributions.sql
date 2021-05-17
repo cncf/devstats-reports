c1f0a21d81ff8e54575de295745d703eac096f91
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-17 07:15:36 +0000

    new reports
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sql/company_project_contributions.sql
@@ -0,0 +1,30 @@
+select
+  sub.company_contributions as contributions,
+  (sub.company_contributions * 100.0) / case sub.{{type}}_contributions when 0 then 1 else sub.{{type}}_contributions end as percent_contributions,
+  (sub.known_contributions * 100.0) / case sub.all_contributions when 0 then 1 else sub.all_contributions end as data_quality
+from (
+  select count(e.id) filter (where af.{{company_name}} = '{{company}}') as company_contributions,
+    count(e.id) filter (where af.{{company_name}} is not null) as known_contributions,
+    count(e.id) as all_contributions
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_actors_affiliations af
+  on
+    e.actor_id = af.actor_id
+    and af.dt_from <= e.created_at
+    and af.dt_to > e.created_at
+  where
+    e.created_at >= '{{dtfrom}}'
+    and e.created_at < '{{dtto}}'
+    and e.repo_id = r.id
+    and e.dup_repo_name = r.name
+    and r.repo_group = '{{project}}'
+    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and e.type in (
+      'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+      'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+    )
+) sub
+;
