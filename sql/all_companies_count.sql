d3ffac634b3f59990e39cf15baf4907e4d254957
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-12-13 13:08:10 +0000

    All orgs stats
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ sql/all_companies_count.sql
@@ -0,0 +1,32 @@
+select
+  count(distinct sub.company_name) as num_companis
+from (
+  select
+    af.company_name
+  from
+    gha_events e,
+    gha_actors_affiliations af
+  where
+    e.actor_id = af.actor_id
+    and af.dt_from <= e.created_at
+    and af.dt_to > e.created_at
+    and af.company_name not in ('Independent', 'Unknown', 'NotFound', '')
+    and e.type in (
+      'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+      'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+    )
+  union select
+    af.company_name
+  from
+    gha_actors_affiliations af,
+    gha_commits c
+  where
+    (
+      c.author_id = af.actor_id
+      or c.committer_id = af.actor_id
+    )
+    and af.dt_from <= c.dup_created_at
+    and af.dt_to > c.dup_created_at
+    and af.company_name not in ('Independent', 'Unknown', 'NotFound', '')
+) sub
+;
