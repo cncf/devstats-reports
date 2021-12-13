e564c50b6a9537b44d1de0890028aa739f1baab5
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-12-13 07:38:52 +0000

    New meric
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ sql/all_contributors_count.sql
@@ -0,0 +1,34 @@
+with contributors as (
+  select dup_committer_login as actor
+  from
+    gha_commits
+  where
+    dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and committer_id is not null
+    and (lower(dup_committer_login) {{exclude_bots}})
+  union select dup_author_login as actor
+  from
+    gha_commits
+  where
+    dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and author_id is not null
+    and (lower(dup_author_login) {{exclude_bots}})
+  union select dup_actor_login as actor
+  from
+    gha_events
+  where
+    created_at >= '{{dtfrom}}'
+    and created_at < '{{dtto}}'
+    and type in (
+      'PushEvent', 'PullRequestEvent', 'IssuesEvent',
+      'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
+    )
+    and (lower(dup_actor_login) {{exclude_bots}})
+)
+select
+  count(distinct actor) as cnt
+from
+  contributors
+;
