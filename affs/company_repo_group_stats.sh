0a7452feea8ef921580b61e80a54894365b2ae85
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-11-24 06:09:05 +0000

    Fix missing script parameter
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- affs/company_repo_group_stats.sh
+++ affs/company_repo_group_stats.sh
@@ -9,4 +9,4 @@ then
   echo "$0: you need to set PG_DB=..., for example PG_DB=allprj"
   exit 2
 fi
-GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_CSVOUT="data/${PG_DB}_company_repo_group_stats.csv" runq sql/company_repo_group_stats.sql {{exclude_bots}} "`cat /etc/gha2db/util_sql/exclude_bots.sql`"
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_CSVOUT="data/${PG_DB}_company_repo_group_stats.csv" runq sql/company_repo_group_stats.sql {{exclude_bots}} "`cat /etc/gha2db/util_sql/exclude_bots.sql`" {{company_name}} company_name
