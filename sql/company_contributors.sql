ab5e6cb24b69e5377fb1b49c227977c5bf39fcf9
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-14 09:25:27 +0000

    Fix div by zero in report
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- sql/company_contributors.sql
+++ sql/company_contributors.sql
@@ -1,7 +1,7 @@
 select
   sub.company_contributors as contributors,
-  (sub.company_contributors * 100.0) / sub.{{type}}_distinct_contributors as percent_contributors,
-  (sub.known_contributors * 100.0) / sub.all_contributors as data_quality
+  (sub.company_contributors * 100.0) / case sub.{{type}}_contributors when 0 then 1 else sub.{{type}}_contributors end as percent_contributors,
+  (sub.known_contributors * 100.0) / case sub.all_contributors when 0 then 1 else sub.all_contributors end as data_quality
 from (
   select count(distinct e.dup_actor_login) filter (where af.{{company_name}} = '{{company}}') as company_contributors,
     count(distinct e.dup_actor_login) filter (where af.{{company_name}} is not null) as known_distinct_contributors,
