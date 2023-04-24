537a02752a9f9230bee801dd31d6427e1aaaf0af
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-24 07:47:03 +0000

    Fix wrong commits metric
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- sql/commits.sql
+++ sql/commits.sql
@@ -1,17 +1,36 @@
--- Should be called with:
--- {{actor}} dup_actor_login
--- {{actor2}} dup_author_login
--- {{actor3}} dup_committer_login
+with commits_data as (
+  select sha,
+    dup_created_at,
+    dup_actor_id as actor_id
+  from
+    gha_commits
+  where
+    dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_actor_login) {{exclude_bots}})
+  union select sha,
+    dup_created_at,
+    author_id as actor_id
+  from
+    gha_commits
+  where
+    author_id is not null
+    and dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_author_login) {{exclude_bots}})
+  union select sha,
+    dup_created_at,
+    committer_id as actor_id
+  from
+    gha_commits
+  where
+    committer_id is not null
+    and dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_committer_login) {{exclude_bots}})
+)
 select
   count(distinct sha) as commits
 from
-  gha_commits
-where
-  dup_created_at >= '{{dtfrom}}'
-  and dup_created_at < '{{dtto}}'
-  and (
-    (lower({{actor}}) {{exclude_bots}})
-    or (lower({{actor2}}) {{exclude_bots}})
-    or (lower({{actor3}}) {{exclude_bots}})
-  )
+  commits_data
 ;
--- sql/commits_by_repo.sql
+++ sql/commits_by_repo.sql
@@ -1,12 +1,42 @@
+with commits_data as (
+  select dup_repo_name,
+    sha,
+    dup_created_at,
+    dup_actor_id as actor_id
+  from
+    gha_commits
+  where
+    dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_actor_login) {{exclude_bots}})
+  union select dup_repo_name,
+    sha,
+    dup_created_at,
+    author_id as actor_id
+  from
+    gha_commits
+  where
+    author_id is not null
+    and dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_author_login) {{exclude_bots}})
+  union select dup_repo_name,
+    sha,
+    dup_created_at,
+    committer_id as actor_id
+  from
+    gha_commits
+  where
+    committer_id is not null
+    and dup_created_at >= '{{dtfrom}}'
+    and dup_created_at < '{{dtto}}'
+    and (lower(dup_committer_login) {{exclude_bots}})
+)
 select
   dup_repo_name as repo,
   count(distinct sha) as commits
 from
-  gha_commits
-where
-  dup_created_at >= '{{dtfrom}}'
-  and dup_created_at < '{{dtto}}'
-  and (lower(dup_{{actor}}_login) {{exclude_bots}})
+  commits_data
 group by
   dup_repo_name
 order by
