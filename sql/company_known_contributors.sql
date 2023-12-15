0cb2707c58ca8464a934a8b42c8360d886499106
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-12-15 10:40:51 +0000

    Updates to report SQLs
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- sql/company_known_contributors.sql
+++ sql/company_known_contributors.sql
@@ -44,46 +44,76 @@ with contributions as (
 ), known_contributions as (
   select distinct c.committer_id as actor_id,
     c.dup_committer_login as actor,
-    c.event_id
+    c.event_id,
+    coalesce(string_agg(distinct an.name, ', '), '-') as names
   from
     gha_commits c,
-    gha_actors_affiliations aa
+    gha_actors_affiliations aa,
+    gha_actors a
+  left join
+    gha_actors_names an
+  on
+    an.actor_id = a.id
   where
     c.committer_id is not null
+    and c.committer_id = a.id
+    and c.dup_committer_login = a.login
     and (lower(c.dup_committer_login) {{exclude_bots}})
     and c.committer_id = aa.actor_id
     and c.dup_created_at >= aa.dt_from
     and c.dup_created_at < aa.dt_to
     and aa.{{company_name}} = '{{company}}'
+  group by
+    1, 2, 3
   union select distinct c.author_id as actor_id,
     c.dup_author_login as actor,
-    c.event_id
+    c.event_id,
+    coalesce(string_agg(distinct an.name, ', '), '-') as names
   from
     gha_commits c,
-    gha_actors_affiliations aa
+    gha_actors_affiliations aa,
+    gha_actors a
+  left join
+    gha_actors_names an
+  on
+    an.actor_id = a.id
   where
     c.author_id is not null
+    and c.author_id = a.id
+    and c.dup_author_login = a.login
     and (lower(c.dup_author_login) {{exclude_bots}})
     and c.author_id = aa.actor_id
     and c.dup_created_at >= aa.dt_from
     and c.dup_created_at < aa.dt_to
     and aa.{{company_name}} = '{{company}}'
+  group by
+    1, 2, 3
   union select distinct e.actor_id,
     e.dup_actor_login as actor,
-    e.id as event_id
+    e.id as event_id,
+    coalesce(string_agg(distinct an.name, ', '), '-') as names
   from
     gha_events e,
-    gha_actors_affiliations aa
+    gha_actors_affiliations aa,
+    gha_actors a
+  left join
+    gha_actors_names an
+  on
+    an.actor_id = a.id
   where
     e.type in (
       'PushEvent', 'PullRequestEvent', 'IssuesEvent', 'PullRequestReviewEvent',
       'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'
     )
+    and e.actor_id = a.id
+    and e.dup_actor_login = a.login
     and (lower(e.dup_actor_login) {{exclude_bots}})
     and e.actor_id = aa.actor_id
     and e.created_at >= aa.dt_from
     and e.created_at < aa.dt_to
     and aa.{{company_name}} = '{{company}}'
+  group by
+    1, 2, 3
 ), contributors as (
   select actor,
     count(distinct event_id) as contributions
@@ -94,12 +124,15 @@ with contributions as (
   order by
     contributions desc
 ), known_contributors as (
-  select actor,
+  select
+    actor,
+    names,
     count(distinct event_id) as contributions
   from
     known_contributions
   group by
-    actor
+    actor,
+    names
   order by
     contributions desc
 ), all_contributions as (
@@ -110,6 +143,7 @@ with contributions as (
 select
   row_number() over cumulative_contributions as rank_number,
   c.actor,
+  c.names,
   c.contributions,
   round((c.contributions * 100.0) / a.cnt, 5) as percent,
   sum(c.contributions) over cumulative_contributions as cumulative_sum,
@@ -120,8 +154,11 @@ from
   all_contributions a
 window
   cumulative_contributions as (
-    order by c.contributions desc, c.actor asc
-    range between unbounded preceding
-    and current row
+    order by
+      c.contributions desc,
+      c.actor asc
+    range between
+      unbounded preceding
+      and current row
   )
 ;
--- sql/raw_company_all.sql
+++ sql/raw_company_all.sql
@@ -1,3 +1,32 @@
 \copy (
-  select a.login, a.name, a.country_name, aa.dt_from as affiliated_from, aa.dt_to as affiliatied_to, min(date(e.created_at)) as first_contribution, max(date(e.created_at)) as last_contribution, count(distinct e.id) as contributions from gha_actors a, gha_actors_affiliations aa left join gha_events e on aa.actor_id = e.actor_id and e.created_at >= aa.dt_from and e.created_at < aa.dt_to where aa.actor_id = a.id and lower(aa.company_name) = '{{company}}' and (e.type is null or e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) group by a.login, a.name, a.country_name, aa.dt_from, aa.dt_to order by contributions desc
+  select
+    a.login,
+    a.name,
+    a.country_name,
+    aa.dt_from as affiliated_from,
+    aa.dt_to as affiliatied_to,
+    min(date(e.created_at)) as first_contribution,
+    max(date(e.created_at)) as last_contribution,
+    count(distinct e.id) as contributions
+  from
+    gha_actors a,
+    gha_actors_affiliations aa
+  left join
+    gha_events e
+  on
+    aa.actor_id = e.actor_id
+    and e.created_at >= aa.dt_from
+    and e.created_at < aa.dt_to
+  where
+    aa.actor_id = a.id
+    and lower(aa.company_name) = '{{company}}'
+    and (e.type is null or e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent'))
+  group by
+    a.login,
+    a.name,
+    a.country_name,
+    aa.dt_from,
+    aa.dt_to
+  order by
+    contributions desc
 ) to '/tmp/all_{{company}}.csv' with csv delimiter ',' header;
