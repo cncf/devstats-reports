8adb972f94dae263a30c31742d8402ac6e2e9e2c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2024-04-08 09:08:25 +0000

    finished report - battle testing now
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/company_project_commits_stats.sql
+++ sql/company_project_commits_stats.sql
@@ -14,9 +14,11 @@ with commits_data as (
     and c.dup_actor_id = af.actor_id
     and af.dt_from <= c.dup_created_at
     and af.dt_to > c.dup_created_at
-    -- and (lower(c.dup_actor_login) {{exclude_bots}})
-    -- and c.dup_created_at >= '{{from}}'
-    -- and c.dup_created_at < '{{to}}'
+    and af.company_name not in ('Independent', '(Robots)', 'CNCF')
+    and rg.repo_group not in ('CNCF')
+    and (lower(c.dup_actor_login) {{exclude_bots}})
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
   union select
     rg.repo_group as project,
     af.company_name as company,
@@ -33,9 +35,11 @@ with commits_data as (
     and af.dt_to > c.dup_created_at
     and c.author_id is not null
     and c.dup_author_login is not null
-    -- and (lower(c.dup_author_login) {{exclude_bots}})
-    -- and c.dup_created_at >= '{{from}}'
-    -- and c.dup_created_at < '{{to}}'
+    and af.company_name not in ('Independent', '(Robots)', 'CNCF')
+    and rg.repo_group not in ('CNCF')
+    and (lower(c.dup_author_login) {{exclude_bots}})
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
   union select
     rg.repo_group as project,
     af.company_name as company,
@@ -52,9 +56,11 @@ with commits_data as (
     and af.dt_to > c.dup_created_at
     and c.committer_id is not null
     and c.dup_committer_login is not null
-    -- and (lower(c.dup_committer_login) {{exclude_bots}})
-    -- and c.dup_created_at >= '{{from}}'
-    -- and c.dup_created_at < '{{to}}'
+    and af.company_name not in ('Independent', '(Robots)', 'CNCF')
+    and rg.repo_group not in ('CNCF')
+    and (lower(c.dup_committer_login) {{exclude_bots}})
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
 ), company_project_commits as (
   select
     company,
@@ -76,22 +82,89 @@ with commits_data as (
   order by
     commits desc
   limit
-    10
-    -- {{limit}}
+    {{limit_companies}}
+), project_commits as (
+  select
+    project,
+    count(distinct sha) as commits
+  from
+    commits_data
+  group by
+    project
+  order by
+    commits desc
+  limit
+    {{limit_projects}}
+), by_company as (
+  select
+    row_number() over (order by i.all_company_commits desc, i.percent_of_all_company_commits desc, i.project asc) as row_num,
+    i.*
+  from (
+    select
+      'by company' as metric,
+      cpc.company,
+      cpc.project,
+      cpc.commits,
+      cc.commits as all_company_commits,
+      pc.commits as all_project_commits,
+      (cpc.commits * 100.0) / cc.commits as percent_of_all_company_commits,
+      (cpc.commits * 100.0) / pc.commits as percent_of_all_project_commits
+    from
+      company_project_commits cpc,
+      company_commits cc,
+      project_commits pc
+    where
+      cpc.company = cc.company
+      and cpc.project = pc.project
+  ) i
+), by_project as (
+  select
+    row_number() over (order by i.all_project_commits desc, i.percent_of_all_project_commits desc, i.project asc) as row_num,
+    i.*
+  from (
+      select
+      'by project' as metric,
+      cpc.company,
+      cpc.project,
+      cpc.commits,
+      cc.commits as all_company_commits,
+      pc.commits as all_project_commits,
+      (cpc.commits * 100.0) / cc.commits as percent_of_all_company_commits,
+      (cpc.commits * 100.0) / pc.commits as percent_of_all_project_commits
+    from
+      company_project_commits cpc,
+      company_commits cc,
+      project_commits pc
+    where
+      cpc.company = cc.company
+      and cpc.project = pc.project
+    ) i
 )
 select
-  cpc.company,
-  cpc.project,
-  cpc.commits,
-  cc.commits as all_commits,
-  (cpc.commits * 100.0) / cc.commits as percent_commits
+  row_num,
+  metric,
+  company,
+  project,
+  commits,
+  all_company_commits,
+  all_project_commits,
+  percent_of_all_company_commits,
+  percent_of_all_project_commits
 from
-  company_project_commits cpc,
-  company_commits cc
-where
-  cpc.company = cc.company
+  by_company
+union select
+  row_num,
+  metric,
+  company,
+  project,
+  commits,
+  all_company_commits,
+  all_project_commits,
+  percent_of_all_company_commits,
+  percent_of_all_project_commits
+from
+  by_project
 order by
-  all_commits desc,
-  cpc.company,
-  percent_commits desc
+  metric,
+  row_num
 ;
