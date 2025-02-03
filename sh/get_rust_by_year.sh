766ca7b84d71b6f1f7e475c7340a315ea73e75e2
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-02-03 17:24:17 +0100

    Fix typo
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- sh/get_rust_by_year.sh
+++ sh/get_rust_by_year.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for y in 2014 2025 2016 2017 2018 2019 2020 2021 2022 2023 2024
+for y in 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024
 do
   wget "https://devstats.cncf.io/backups/rust_projects_${y}.csv"
 done
